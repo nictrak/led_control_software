f7ee9d70e94b99d4090a3b8af3b549974b37e210
nictrak <narin14175@gmail.com>
2020-06-25 14:53:27 +0700

    init push

---
+++ Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../control_software_1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../control_software_1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../control_software_1_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../control_software_1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../control_software_1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../control_software_1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../control_software_1_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../control_software_1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
